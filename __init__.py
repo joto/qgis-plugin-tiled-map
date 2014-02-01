15e47b531d1bb5fab72690135b017ef5cba0e4d2
Jochen Topf <jochen@topf.org>
2014-02-01 17:24:55 +0100

    Updates for QGIS 2.0

--- __init__.py
+++ __init__.py
@@ -1,14 +1,4 @@
 #!python
-def name():
- return "TiledMap"
-def description():
- return "Add tiled web maps as raster layer."
-def version():
- return "Version 1.0"
-def qgisMinimumVersion(): 
- return "1.8"
-def authorName():
- return "Jochen Topf"
 def classFactory(iface):
  from plugin import TiledMapPlugin
  return TiledMapPlugin(iface)
